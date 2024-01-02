bfc9e008a2e0b46c3c824f9274cab5da3880f528
cletqui <antoine.quillivic@gmail.com>
2024-01-02 16:56:49 +0100

    v0.3.0 Implement Footer

--- src/containers/Body/Output/Output.jsx
+++ src/containers/Body/Output/Output.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+import { Section } from "../../../components/layout/section/Section";
+
 export const Output = React.forwardRef(({ result }, ref) => {
   return (
     <div className="Output" ref={ref}>
-      {result ? <h1>Output</h1> : null}
+      {result ? (
+        <div>
+          <h1>Output</h1>
+          {Object.entries(result).map(([key, value]) => {
+            return <Section key={key} type={key} content={value} />;
+          })}
+        </div>
+      ) : null}
     </div>
   );
 });
--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,1 +1,27 @@
-export const Footer = () => {};
+import { SiGithub, SiBuymeacoffee } from "react-icons/si";
+
+export const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div>
+        <a
+          href="https://github.com/cletqui/mail"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiGithub />
+        </a>
+      </div>
+
+      <div>
+        <a
+          href="https://www.buymeacoffee.com/cletqui"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiBuymeacoffee />
+        </a>
+      </div>
+    </footer>
+  );
+};
