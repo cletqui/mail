fe63b95b0dbf876a7143d7a1bee6de1f4c23ad72
cletqui <antoine.quillivic@gmail.com>
2024-01-17 11:41:13 +0100

    check if the list content is valid

--- src/components/layout/List.jsx
+++ src/components/layout/List.jsx
@@ -1,13 +1,17 @@
 import { capitalize } from "../../utils/helpers/capitalize";
 
 export const List = ({ listKey, list }) => {
+  const isValid = (string) => {
+    return string && string.length > 0;
+  };
+
   return (
     <div className="List">
       <ul key={listKey}>
         {Object.entries(list).map(([key, value], index) => {
-          return (
+          return isValid(value) ? (
             <li key={`${key}-${index}`}>{`${capitalize(key)}: ${value}`}</li>
-          );
+          ) : null;
         })}
       </ul>
     </div>
